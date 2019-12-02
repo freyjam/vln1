6a51d18be2e5ef59a0f26b61171479f33bf904b8
njalsson <njalsson1337@gmail.com>
2019-12-02 13:26:18 +0000

    added hierarchy to classes

--- src/data/classes.py
+++ src/data/classes.py
@@ -11,20 +11,53 @@ class Airplane:
 		pass
 
 
-class pilotNode:
-	def __init__(self):
-		pass
+class Crew:
+	def __init__(self, name, ssn, address, phone, homephone):
+		self.name = name
+		self.ssn = ssn
+		self.address = address
+		self.phone = phone
+		self.homephone = homephone
 
-class flightAttendantNode:
-	def __init__(self):
-		pass
+
+class pilotNode(Crew):
+	def __init__(self, name , ssn , address , phone , homephone, rank = None, privileges = None ):
+		super().__init__(name , ssn , address , phone , homephone)
+		self.rank = rank
+		self.privileges = privileges
+
+	def __str__(self):
+		return str(self.ssn)
+
+class flightAttendantNode(Crew):
+	def __init__(self, name, ssn, address, phone, homephone, isboss, rank):
+		super().__init__(name, ssn, address, phone, homephone)
+		self.isboss = isboss
+		self.rank = rank
 
 
 class peeps: ## store people in a hash map
 	def __init__(self):
-		self.buckets = [ [16] ]
+		self.cap = 16
+		self.buckets = [ [] ] * self.cap
 
 	def insertpilot(self, name, ssn, address, phone, homephone):
 		node = pilotNode(name, ssn, address, phone, homephone)
-		index = hash(name)
+		index = hash(name) % self.cap
+		if node in self.bucket[index]:
+			pass
+			# update
+		else:
+			self.bucket[index].append(node)
+
+	def contains(self, name, ssn):
+		out = False
+		index = hash(name) % self.cap
+		for x in self.buckets[index]:
+			if x.ssn is ssn:
+				out = True
+
+		return out
+
+
 
