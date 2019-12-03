608b544c21b4f155e1c2d98c2aaa9fe277395580
Eva Sól Pétursdóttir <evasolpetursdottir@Evas-MacBook-Air-5.local>
2019-12-03 16:36:33 +0000

    Færðum modelklasa í sér möppu og endurskýrðum möppurnar til að skipulagið væri betra

---
+++ src/modelClasses/crewClass.py
@@ -0,0 +1,11 @@
+class Crew:
+	def __init__(self, ssn, name, role, rank, license, address, phone, homephone, email):
+		self.ssn = ssn
+		self.name = name
+		self.role = role
+		self.rank = rank
+		self.license = license
+		self.address = address
+		self.phone = phone
+		self.homephone = homephone
+		self.email = email
