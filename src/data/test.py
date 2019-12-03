009b9ca6e8f2c2cfd0363c80b916feed89a4287b
DESKTOP-CC9ITHR\Katrin Lilja <katrin-97@hotmail.com>
2019-12-03 15:12:17 +0000

    engar breytingar

--- src/data/test.py
+++ src/data/test.py
@@ -3,7 +3,7 @@ import csv
 
 Datalayerapi = classes.DataLayerAPI()
 
-with open('csv/Crew.csv') as csv_file:
+with open('Crew.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
