42b8d3f45ed5286d390659c4f816e3a3eb5fa9af
Seigla <laramargret3430@gmail.com>
2019-12-06 13:52:35 +0000

    More space for names, work schedule attempt and removed a menu function that isn't needed

--- src/UILayer/userinterface.py
+++ src/UILayer/userinterface.py
@@ -6,29 +6,21 @@ from LogicLayer.logic_test import LogicLayerAPI
 class Output:
     def __init__(self):
         self.printer = LogicLayerAPI()
-
-    def printMenu(self, menu):
-        #Needs an iterable format of menu options.
-        output = "What would you like to do?\n"
-        i = 1                                       #Initialize number of options, first option is marked 1.
-        for option in menu:
-            output += "\n" + "{}. {}".format(i, option)
-            i += 1
-        output += "\n\nb - Go Back\nm - Main Menu"
-        print(output)
    
     def printAllStaff(self):            #Gætum hugsanlega notað þetta sama fall í að prenta filteraðan staff lista.
+        #Meira pláss fyrir nafnið
         a = LogicLayerAPI()
         ret_str = "####\nAll Staff\n####"
-        header = "\n\n{:<15} {:<15} {:<15} {:<15} {:<20} {:<13} {:<25} {:<15}".format("Name", "SSN", "Address", "Phone number", "E-mail address", "Role", "Rank", "License")   
+        header = "\n\n{:<20} {:<15} {:<15} {:<15} {:<20} {:<13} {:<25} {:<15}".format("Name", "SSN", "Address", "Phone number", "E-mail address", "Role", "Rank", "License")   
         ret_str += header
         ret_str += "\n" + "-" * len(ret_str)            #Seperates header from data
         for member in a.sortAllCrewAlpha():             #Laga þegar við tengjum við LL
-            ret_str += "\n{:<15} {:<15} {:<15} {:<15} {:<20} {:<13} {:<25} {:<15}".format\
+            ret_str += "\n{:<20} {:<15} {:<15} {:<15} {:<20} {:<13} {:<25} {:<15}".format\
                 (member.name, member.ssn, member.address, member.phone, member.email, member.role, member.rank, member.license)
         print(ret_str)
    
-    def printStaffMember(self):
+    def printStaffMember(self, ssn):
+        staffMember = self.printer.getCrewMemberBySsn(ssn)
         ret_str = "####\n{}\n####".format(staffMember.name)
         ret_str += "\n\nName: {}\nSocial Security Number: {}\nAddress: {}\nPhone Number: {}\nE-mail address: {}\nRole: {}\nRank: {}\nLicense: {}".format\
             (staffMember.name, staffMember.snn, staffMember.address, staffMember.phone, staffMember.email, staffMember.role, staffMember.rank, staffMember.license)
@@ -37,20 +29,20 @@ class Output:
     def printUnavailableStaff(self):
         #Birta fleiri/færri upplýsingar?
         ret_str = "####\nUnavailable Staff\n####"
-        header = "\n\n{:<15} {:<15} {:<25} {:<15} {:<15}".format("Name", "SSN", "Rank", "Destination", "Becomes Available")   
+        header = "\n\n{:<20} {:<15} {:<25} {:<15} {:<15}".format("Name", "SSN", "Rank", "Destination", "Becomes Available")   
         ret_str += header
         ret_str += "\n" + "-" * (len(ret_str) - 20)            #Seperates header from data
         for staff in unavailable_staff_list:
-            ret_str += "\n{:<15} {:<15} {:<25} {:<15} {:<15}".format(staff.name, staff.ssn, staff.rank, staff.destination, staff.freedate)
+            ret_str += "\n{:<20} {:<15} {:<25} {:<15} {:<15}".format(staff.name, staff.ssn, staff.rank, staff.destination, staff.freedate)
         print(ret_str)
     
     def printAvailableStaff(self):
         #Birta fleiri upplýsingar? T.d. license
         ret_str = "####\nAvailable Staff\n####"
-        header = "\n\n{:<15} {:<15} {:<25}".format("Name", "SSN", "Rank")
+        header = "\n\n{:<20} {:<15} {:<25}".format("Name", "SSN", "Rank")
         ret_str += header + "\n" + "-" * (len(ret_str) - 20)
         for staff in available_staff_list:
-            ret_str += "\n{:<15} {:<15} {:<25}".format(staff.name, staff.ssn, staff.rank)
+            ret_str += "\n{:<20} {:<15} {:<25}".format(staff.name, staff.ssn, staff.rank)
         print(ret_str)
 
     def printDestinations(self):
@@ -62,20 +54,22 @@ class Output:
    
     def printAirplanes(self):
         ret_str = "####\nAirplanes\n####"
-        header = "\n\n{:<15}{:<15}{:<20}{:<15}".format("Name", "Model", "Manufacturer", "Capacity")
+        header = "\n\n{:<15}{:<15}{:<15}".format("Insignia", "Type", "Capacity")
         ret_str += header
         ret_str += "\n" + "-" * (len(ret_str) - 20) #-20 to make the line align better with the header
         for airplane in airplaneList:               #Vantar fall frá logic layer
-            ret_str += "\n{:<15}{:<15}{:<20}{:<10}".format(airplane.name, airplane.model, airplane.manufacturer, airplane.capacity)
+            ret_str += "\n{:<15}{:<15}{:<10}".format(airplane.insignia, airplane.typeid, airplane.capacity)
         print(ret_str)
  
     def printWorkSchedule(self):
-        #Needs both the desired staff member and their work schedule from LL
+        #Gets a tuple consisting of an instance of a staff member and their designated voyages
         ret_str = "####\nWork Schedule\n####\n"
-        staff_info = "\n{:<15} {:<15} {:<15} {:<30}".format(staff.name, staff.ssn, staff.role, staff.rank)
-        frame = "\n" + "=" * (len(staff_info) - 5)
         schedule = ""
-        for entry in work_plan:
+        staff = work_plan_tuple[0]
+        staff_info = "\n{:<20} {:<15} {:<15} {:<30}".format(staff.name, staff.ssn, staff.role, staff.rank)
+        frame = "\n" + "=" * (len(staff_info) - 5)
+        plan = work_plan_tuple[1]
+        for entry in plan:
             schedule += "\n{:<15} {:<15} {:<11} {:<5}\n{:>43} {:>6}".format(entry.ddmmyy, entry.destination, "Departure: ", entry.departuretime, "Arrival: ", entry.arrivaltime)
         ret_str += frame + staff_info + frame + schedule
         print(ret_str)
@@ -92,7 +86,6 @@ class Output:
             inbound_info = "\n\t{:<11} {:<6} {:<10}\n\t{:<11} {:<6} {:<10}".format("Departure: ", voyage.indeptime, voyage.indepdate, "Arrival: ", voyage.inarrtime, voyage.inarrdate)
             crew = "\n   Crew: "
             for member in voyage_crew:  #For every member in the voyage's crew
-                # Hér gæti þurft að kalla á fallið sem leitar að crew members eftir kt.
                 crew += "\n\t{}, {}".format(member.name, member.rank)
             ret_str += voytitlestatus + outbound + outbound_info + inbound + inbound_info + crew
         print(ret_str)
