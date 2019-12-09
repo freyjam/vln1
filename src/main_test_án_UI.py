2c9b7112cd64c8306370de5ccb895eb64347d1b2
Eva Sól Pétursdóttir <evasolpetursdottir@Evas-MacBook-Air-5.local>
2019-12-09 11:46:24 +0000

    Laga fall sem finnur NotWorkingCrew

--- src/main_test_án_UI.py
+++ src/main_test_án_UI.py
@@ -2,22 +2,12 @@ from LogicLayer.logic_test import LogicLayerAPI
 
 a = LogicLayerAPI()
 
-#for x in a.listOfAllAircraftsWithState():
-#    print(x.state, x.availableAt, x.destination, x.numberOfFlight)
-
-print('Working')
+print('##### WORKING ######')
 
 for x in a.getAllCrewWorking('21/12/2019'):
     print(x.name, x.ssn, x.destination)
 
-print('Not working')
+print('###### NOT WORKING ######')
 
 for x in a.getAllCrewNotWorking('21/12/2019'):
-    print(x.name, x.ssn, x.destination)
-
-print('Schedule for 1900769521')
-
-crewMember, voyages = a.getWorkScheduleForCrewMember('1900769521', '19/12/2019', '24/12/2019')
-print('Name: {} SSN: {}'.format(crewMember.name, crewMember.ssn))
-for x in voyages:
-    print('Destination: {} Departure: {}  Arrival: {}'.format(x.destinationAirport, a.changeFromIsoTimeFormat(x.departure), a.changeFromIsoTimeFormat(x.arrival)))
+     print(x.name, x.ssn)
