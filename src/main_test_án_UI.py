8763cae0a4e1f7458b92a72dc8187fe8021cada6
Eva Sól Pétursdóttir <evasolpetursdottir@Evas-MacBook-Air-5.local>
2019-12-09 13:22:24 +0000

    2 föll sem tengjast því að manna ferð í LL

--- src/main_test_án_UI.py
+++ src/main_test_án_UI.py
@@ -2,12 +2,5 @@ from LogicLayer.logic_test import LogicLayerAPI
 
 a = LogicLayerAPI()
 
-print('##### WORKING ######')
-
-for x in a.getAllCrewWorking('21/12/2019'):
-    print(x.name, x.ssn, x.destination)
-
-print('###### NOT WORKING ######')
-
-for x in a.getAllCrewNotWorking('21/12/2019'):
-     print(x.name, x.ssn)
+for x in a.getAllPilotsAfterLicenseOnAircraft('TF-EPG', 'Copilot'):
+    print(x.name, x.ssn, x.role, x.rank)
